106eb7c77f4cfe54604bffbf360b1dd688871f91
Bryan Loyd <bryan.loyd@outlook.com>
2021-11-25 15:35:20 -0600

    Updates

--- src/components/CardComponent.js
+++ src/components/CardComponent.js
@@ -1,5 +1,4 @@
 import React from "react";
-import image from "../images/image-equilibrium.jpg";
 import eth from "../images/icon-ethereum.svg";
 import clock from "../images/icon-clock.svg";
 import avatar from "../images/image-avatar.png";
@@ -7,9 +6,7 @@ import avatar from "../images/image-avatar.png";
 const CardComponent = () => {
   return (
     <div className="Card">
-      <div>
-        <img src={image} alt="" className="Card_image" />
-      </div>
+      <div className="Card_image"></div>
       <div className="Card_content">
         <h1>Equilibrium #3429</h1>
         <p>Our Equilibrium collection promotes balance and calm.</p>
